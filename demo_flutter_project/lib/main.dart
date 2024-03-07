5b2eeb784fe093905d866fbc5e83332d360159da
Sunil Kumar Yadav <yadavsunilsky21@gmail.com>
2024-03-07 22:44:45 +0530

    Aded Scaffold in HomePage widget

--- demo_flutter_project/lib/main.dart
+++ demo_flutter_project/lib/main.dart
@@ -1,21 +1,18 @@
 // Basic fluter application using dart.
 
 //import 'package:flutter/widgets.dart';
-
-import 'package:flutter/material.dart';
 //import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 
 void main() {
 
-  // runApp() takes three types of app.
-  // 1. WidgetApp, 2. MaterialApp, 3.CupertinoApp 
-  // WidgetApp are low level component and does not have many support directly.
   runApp(MaterialApp(
-    home: HomePage()
+    home: HomePage(),
+    theme: ThemeData(
+      primarySwatch: Colors.purple,
+    ),
   ));
-
-  print("Successfully launched flutter app!!!");
 }
 
 class HomePage extends StatelessWidget {
@@ -23,9 +20,11 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blueAccent,
-      child: Text("Hello Flutter App")
-      );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Demo App!"),
+        backgroundColor: Colors.greenAccent,
+      ),
+    );
   }
 }
