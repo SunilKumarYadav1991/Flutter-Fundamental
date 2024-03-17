d6d6518f310b27493c9386a025a43c0224e9270c
Sunil Kumar Yadav <yadavsunilsky21@gmail.com>
2024-03-17 10:32:56 +0530

    Added image in asset folder and updated pubspec.
    Used padding and wrapped card under it for simplified viewing

--- demo_flutter_project/lib/main.dart
+++ demo_flutter_project/lib/main.dart
@@ -4,13 +4,12 @@
 //import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
 void main() {
-
   runApp(MaterialApp(
     home: HomePage(),
     theme: ThemeData(
-      primarySwatch: Colors.purple,                             /* Does not work as per tutorial. Need to debug further */
+      primarySwatch: Colors
+          .purple, /* Does not work as per tutorial. Need to debug further */
     ),
   ));
 }
@@ -21,47 +20,53 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Chapter 3 App!"),
-        backgroundColor: Colors.greenAccent,                     /* Need to add color as default banner is white color */
-      ),
-      body: Center( 
-        child: Container(                                             /* body: Container() Centre will force all widgets under body in centre of screen*/
-          height: 100,
-          width: 100,                                      
-          color: Colors.cyan,        
+        appBar: AppBar(
+          title: Text("Chapter 3 App!"),
+          backgroundColor: Colors
+              .greenAccent, /* Need to add color as default banner is white color */
         ),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            UserAccountsDrawerHeader(
-              accountName: Text("Sunil Yadav"), 
-              accountEmail: Text("example@gmail.com"),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage("https://images.unsplash.com/photo-1599566150163-29194dcaad36?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGZhY2VzfGVufDB8fDB8fHww"),
-              )
+        body: Center(
+            child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Card(
+            child: Column(
+              children: [
+                Image.asset(
+                    "assets/codingImage.jpg"), /* Using workspace image. Image.asset have many other properties */
+              ],
             ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text("Sunil Yadav"),
-            subtitle: Text("Sr. Tech Lead"),
-            trailing: Icon(Icons.edit),
-            onTap: () {},
-            ),
-          ListTile(
-            leading: Icon(Icons.email),
-            title: Text("Email"),
-            subtitle: Text("example@gmail.com"),
-            trailing: Icon(Icons.edit),
-            ),
-          ],
+          ),
+        )),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              UserAccountsDrawerHeader(
+                  accountName: Text("Sunil Yadav"),
+                  accountEmail: Text("example@gmail.com"),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://images.unsplash.com/photo-1599566150163-29194dcaad36?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGZhY2VzfGVufDB8fDB8fHww"),
+                  )),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text("Sunil Yadav"),
+                subtitle: Text("Sr. Tech Lead"),
+                trailing: Icon(Icons.edit),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.email),
+                title: Text("Email"),
+                subtitle: Text("example@gmail.com"),
+                trailing: Icon(Icons.edit),
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed:() {},   /*onPressed is of type void Function()?. Currently its set to empty.*/
-        child: Icon(Icons.edit),
-      )
-    );
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          /*onPressed is of type void Function()?. Currently its set to empty.*/
+          child: Icon(Icons.edit),
+        ));
   }
 }
